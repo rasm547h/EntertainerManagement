a37102981a038852bde69fd838768266fcf434c2
Rasmus Larsson <Rasmus@RLL-PC>
2019-02-10 17:06:54 +0100

    Multiplicity fixed in agentEntertainerContract and EntertainerGroup

--- EntertainerManagement/EntertainerGroup.sql
+++ EntertainerManagement/EntertainerGroup.sql
@@ -2,14 +2,14 @@
 (
 	[EntertainerGroup_ID] int IDENTITY(1,1) NOT NULL ,
     [Entertainer_name] varchar(100)  NOT NULL ,
-    [FK_agentID] int  NULL ,
+    FK_ContractID int  NULL ,
     [FK_styleID] int  NULL ,
     [EntertainmentGroup_active] bit  NOT NULL DEFAULT(0),
     CONSTRAINT [PK_EntertainerGroup] PRIMARY KEY CLUSTERED (
         [EntertainerGroup_ID] ASC
     ),
-	CONSTRAINT [eGroup_FK_agentID] FOREIGN KEY (FK_agentID)
-		REFERENCES AgentEntertainerContract (FK_agentID),
+	CONSTRAINT [eGroup_FK_agentID] FOREIGN KEY (FK_ContractID)
+		REFERENCES AgentEntertainerContract (Contract_ID),
 	CONSTRAINT [FK_style] FOREIGN KEY (FK_styleID)
 		REFERENCES Style (Style_ID)
 );
