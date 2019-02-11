36ce74301f6bade91491a6ab21a789834bd4a3f2
Mahdi Petersen <31881991+Mart224s@users.noreply.github.com>
2019-02-11 18:11:46 +0100

    Refactored Agency

--- EntertainerManagement/Agency.sql
+++ EntertainerManagement/Agency.sql
@@ -1,8 +1,13 @@
 ﻿CREATE TABLE [Agency] (
-    [Agency_ID] int IDENTITY(1,1) NOT NULL ,
-    [Agency_name] varchar(100)  NOT NULL ,
+    [Agency_ID] INT IDENTITY(1,1) NOT NULL ,
+    [Agency_name] VARCHAR(100)  NOT NULL )
+	GO
+
+	ALTER TABLE dbo.Agency ADD
     CONSTRAINT [PK_Agency] PRIMARY KEY CLUSTERED (
-        [Agency_ID] ASC
-    ),
+        [Agency_ID] )
+    GO
+
+	ALTER TABLE dbo.Agency ADD
 	CONSTRAINT [Agency_name] CHECK(LEN([Agency_name]) > 0)
-)
+	GO
