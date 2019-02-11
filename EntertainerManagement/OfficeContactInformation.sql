de3030e857eef8f89a898a8151e24adfc9af61d0
Rasmus Larsson <Rasmus@RLL-PC>
2019-02-11 12:35:25 +0100

    Tabel.OfficeContactionInformation Refactored

--- EntertainerManagement/OfficeContactInformation.sql
+++ EntertainerManagement/OfficeContactInformation.sql
@@ -5,5 +5,10 @@
 	ContactInfo_phoneNoExt varchar(11) null,
 	ContactInfo_email varchar(254) null,
 	ContactInfo_type char(1) not null,
+	
+);
+GO
+
+ALTER TABLE OfficeContactInformation ADD
 	CONSTRAINT [PK_ContactInfo_ID] PRIMARY KEY (ContactInfo_ID)
-)
+GO
