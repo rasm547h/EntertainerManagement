8a26227565a469ecd061246a43cf581d10c61dcb
Rasmus Larsson <Rasmus@RLL-PC>
2019-02-11 11:44:22 +0100

    Table/EntertainerGroup Refactored: Create and constrains

--- EntertainerManagement/EntertainerGroup.sql
+++ EntertainerManagement/EntertainerGroup.sql
@@ -1,16 +1,22 @@
 ﻿CREATE TABLE [dbo].[EntertainerGroup]
 (
-	[EntertainerGroup_ID] int IDENTITY(1,1) NOT NULL ,
+	[EntertainerGroup_ID] int IDENTITY(1,1) NOT NULL,
     [Entertainer_name] varchar(100)  NOT NULL ,
     FK_ContractID int  NULL ,
     [FK_styleID] int  NULL ,
     [EntertainmentGroup_active] bit  NOT NULL DEFAULT(0),
-    CONSTRAINT [PK_EntertainerGroup] PRIMARY KEY CLUSTERED (
-        [EntertainerGroup_ID] ASC
-    ),
-	CONSTRAINT [eGroup_FK_agentID] FOREIGN KEY (FK_ContractID)
-		REFERENCES AgentEntertainerContract (Contract_ID),
-	CONSTRAINT [FK_style] FOREIGN KEY (FK_styleID)
-		REFERENCES Style (Style_ID)
+    
 );
+GO
 
+ALTER TABLE EntertainerGroup ADD
+	CONSTRAINT [PK_EntertainerGroup] PRIMARY KEY CLUSTERED ( [EntertainerGroup_ID] ASC );
+GO
+
+ALTER TABLE EntertainerGroup ADD
+	CONSTRAINT [eGroup_FK_agentID] FOREIGN KEY (FK_ContractID) REFERENCES AgentEntertainerContract (Contract_ID)
+GO
+
+ALTER TABLE EntertainerGroup ADD
+	CONSTRAINT [FK_style] FOREIGN KEY (FK_styleID) REFERENCES Style (Style_ID)
+GO
