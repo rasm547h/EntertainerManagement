b2f3147752ffc9d3998751fef09d499bd7926c29
Rasmus Larsson <Rasmus@RLL-PC>
2019-02-11 12:23:13 +0100

    eGroupMember refactored

--- EntertainerManagement/eGroupMember.sql
+++ EntertainerManagement/eGroupMember.sql
@@ -2,8 +2,14 @@
 (
 	[FK_eGroupID] INT NOT NULL,
 	[FK_eMemberID] INT NOT NULL,
-	CONSTRAINT [FK_eGroupID] FOREIGN KEY (FK_eGroupID)
-		REFERENCES EntertainerGroup (EntertainerGroup_ID),
-	CONSTRAINT [FK_eMemberID] FOREIGN KEY (FK_eMemberID)
-		REFERENCES EntertainerMember (eMember_ID)
-)
+	
+);
+GO
+
+ALTER TABLE eGroupMember ADD
+CONSTRAINT [FK_eGroupID] FOREIGN KEY (FK_eGroupID) REFERENCES EntertainerGroup (EntertainerGroup_ID)
+GO
+
+ALTER TABLE eGroupMember ADD
+	CONSTRAINT [FK_eMemberID] FOREIGN KEY (FK_eMemberID) REFERENCES EntertainerMember (eMember_ID)
+GO
