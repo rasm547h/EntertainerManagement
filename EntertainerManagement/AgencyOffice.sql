b80afc2e46cd23f25a266d82cfdc79ccbbdec346
Mahdi Petersen <31881991+Mart224s@users.noreply.github.com>
2019-02-12 12:02:56 +0100

    Refactored AgencyOffice

--- EntertainerManagement/AgencyOffice.sql
+++ EntertainerManagement/AgencyOffice.sql
@@ -1,13 +1,25 @@
 ﻿CREATE TABLE [AgencyOffice] (
     [FK_agencyID] int NOT NULL ,
     [FK_addressID] INT NOT NULL ,
-	[FK_ContactInfoID] int not null,
+	[FK_ContactInfoID] INT NOT NULL)
+	GO
+
+	ALTER TABLE dbo.AgencyOffice ADD
     CONSTRAINT [FK_agencyID] FOREIGN KEY (FK_agencyID)
-		REFERENCES Agency (Agency_ID) ON DELETE NO ACTION,
+	REFERENCES Agency (Agency_ID) ON DELETE NO ACTION
+	GO
+
+	ALTER TABLE dbo.AgencyOffice ADD
 	CONSTRAINT [FK_addressID] FOREIGN KEY (FK_addressID)
-		REFERENCES Address (Address_ID) ON DELETE CASCADE,
-	CONSTRAINT [PK_AgencyOffice] PRIMARY KEY CLUSTERED (
-        FK_addressID ),
+	REFERENCES Address (Address_ID) ON DELETE CASCADE
+	GO
+
+	ALTER TABLE dbo.AgencyOffice ADD
+	CONSTRAINT [PK_AgencyOffice] PRIMARY KEY CLUSTERED
+    (FK_addressID )
+	GO
+
+    ALTER TABLE dbo.AgencyOffice ADD
 	CONSTRAINT [FK_ContactInfoID] FOREIGN KEY (FK_ContactInfoID)
-		REFERENCES OfficeContactInformation (ContactInfo_ID) ON DELETE NO ACTION
-);
+	REFERENCES OfficeContactInformation (ContactInfo_ID) ON DELETE NO ACTION
+	GO
