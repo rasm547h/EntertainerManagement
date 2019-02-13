80b72fc18d66f146d53075978ae7112936bb2916
Rasmus Larsson <Rasmus@RLL-PC>
2019-02-13 21:32:41 +0100

    Stored Procedure for GetAgentWithEntertainerByName Created

---
+++ EntertainerManagement/GetAgentWithEntertainersByName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetAgentWithEntertainersByName]
+	@fullNameParam NVARCHAR(50)
+AS
+BEGIN
+	SELECT *
+	FROM dbo.agentEntertainer
+	WHERE (dbo.agentEntertainer.Agent_fName + ' ' + dbo.agentEntertainer.Agent_lName) = @fullNameParam;
+END
+GO
