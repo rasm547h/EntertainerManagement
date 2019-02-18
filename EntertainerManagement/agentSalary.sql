a059d2e618eb06a099b540551aeca8787195e69c
Mahdi Petersen <31881991+Mart224s@users.noreply.github.com>
2019-02-18 09:41:58 +0100

    Created View for AgentSalary

---
+++ EntertainerManagement/agentSalary.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[agentSalary]
+	AS SELECT Agent.Agent_fName, Agent.Agent_lName, Agent_salary
+	FROM Agent
+	INNER JOIN(SELECT AgencyOffice.FK_addressID, AgencyOffice.FK_ContactInfoID
+	FROM AgencyOffice) as Agency
+	ON Agent.Agent_ID = Agent.FK_agencyOfficeID;
+	GO
